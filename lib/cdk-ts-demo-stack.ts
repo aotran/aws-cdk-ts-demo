4ae536e138d3a91d20e4cd125c63deb53feb3a48
Alex Eichhorn <alexandraeichhorn@outlook.com>
2021-08-23 14:42:13 -0500

    Adds custom VPC construct and S3 bucket to stack

--- lib/cdk-ts-demo-stack.ts
+++ lib/cdk-ts-demo-stack.ts
@@ -1,9 +1,23 @@
 import * as cdk from '@aws-cdk/core';
+import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3';
+import { Networking } from './networking';
 
 export class CdkTsDemoStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create POs Bucket
+    const bucket = new Bucket(this, 'PurchaseOrdersBucket', {
+      encryption: BucketEncryption.S3_MANAGED
+    });
+    // Create PO Bucket Name export if needed for other stacks
+    new cdk.CfnOutput(this, 'POsBucketNameExport', {
+      value: bucket.bucketName,
+      exportName: 'POsBucket'
+    });
+    // Create VPC from custom template
+    new Networking(this, 'NetworkingConstruct', {
+      maxAzs: 2
+    })
   }
 }
